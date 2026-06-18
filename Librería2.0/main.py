d79cf58166c99faeff30c020a0ae936c4931b611
Masterpro32 <N00481308@upn.pe>
2026-06-18 01:01:04 -0500

    Refactor main.py to include main function

--- Librería2.0/main.py
+++ Librería2.0/main.py
@@ -1,4 +1,8 @@
 from interfaz import VentanaPrincipal
 
-app = VentanaPrincipal()
-app.ejecutar()
+def main():
+    app = VentanaPrincipal()
+    app.ejecutar()
+  
+if __name__ == "__main__":
+    main()  
